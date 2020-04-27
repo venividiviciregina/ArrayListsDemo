a7520869cc69d03687da07aae1f52972ce385ddf
venividiviciregina <venividiviciregina@gmail.com>
2020-04-27 17:03:15 +0200

    Add public void testGetElement() { und public boolean remove(E e) {

---
+++ src/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    //Arrays:
+    //adding is not convenient when index > length
+    //deletion reguires recreation or putting last element to the place of deleted
+    //update, search
+    //addFirst
+}
